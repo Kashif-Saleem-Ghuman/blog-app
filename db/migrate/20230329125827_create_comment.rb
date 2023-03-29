41eff1e0597a7faf20ceed1fd60d830bccfebc98
unknown <kashif_saleem_23@hotmail.com>
2023-03-29 18:04:01 +0500

    Created tables using migration

---
+++ db/migrate/20230329125827_create_comment.rb
@@ -0,0 +1,11 @@
+class CreateComment < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.integer :user_id
+      t.integer :post_id
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
