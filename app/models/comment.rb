5df95a27d7a32459810869a43c9317f2391ca407
unknown <kashif_saleem_23@hotmail.com>
2023-03-30 16:00:17 +0500

    Fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
   belongs_to :post
 
   after_create :update_comments_counter
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  has_many :comments, foreign_key: "post_id"
-  has_many :likes, foreign_key: "post_id"
-  belongs_to :author, class_name: "User"
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User'
 
   after_save :update_posts_counter
 
