30fd2a873845c29bc3fdb9222dc18c4e0ea8db34
unknown <kashif_saleem_23@hotmail.com>
2023-03-30 15:46:39 +0500

    Change default values of counters to 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_30_094819) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_30_103544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,11 +36,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_30_094819) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "comments_counter"
+    t.integer "comments_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
-    t.integer "likes_counter"
+    t.integer "likes_counter", default: 0
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_30_094819) do
     t.string "name"
     t.string "photo"
     t.text "Bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
