290b15c43a4e03e31b1bb4f457a764ded3a877d1
unknown <kashif_saleem_23@hotmail.com>
2023-03-31 14:36:37 +0500

    Add tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
 
   validates :name, presence: true
-  validates :posts_couter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   
 
   def recent_posts
-    author.posts.order(created_at: :desc).limit(3)
+    posts.order(created_at: :desc).limit(3)
   end
 
   
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: "John", Bio:"Anything", photo:"https://t3.ftcdn.net/jpg/02/47/40/98/360_F_247409832_pPugfgU5cKLsrH5OCJRMn5JTcy2L1Rrg.jpg") }
+  subject { User.new(name: "John", photo:"https://t3.ftcdn.net/jpg/02/47/40/98/360_F_247409832_pPugfgU5cKLsrH5OCJRMn5JTcy2L1Rrg.jpg", Bio:"Anything",  posts_counter: 5) }
 
   before { subject.save }
 
@@ -18,5 +18,17 @@ RSpec.describe User, type: :model do
   it 'posts_counter should be greater than or equal to 0' do
     subject.posts_counter = -1
     expect(subject).to_not be_valid
+  end
+
+  describe '#recent_posts' do
+    before do
+      6.times do |p|
+        Post.create(title: "Post #{p}", text: "Text #{p}",comments_counter: 10, author_id: subject.id, likes_counter: 10)
+      end
+    end
+  end
+
+  it 'should return the 3 most recent posts' do
+    expect(subject.recent_posts).to eq(subject.posts.order(created_at: :desc).limit(3))
   end
 end
