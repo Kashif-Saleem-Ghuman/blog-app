b1e2e5d5d95fea77eaad7f4245772a0c3b16fe8f
unknown <kashif_saleem_23@hotmail.com>
2023-03-31 12:38:33 +0500

    Configure database.yml for rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-Rsepc.describe User, type: :model do
+RSpec.describe User, type: :model do
   subject { User.new(name: "John", Bio:"Anything", photo:"https://t3.ftcdn.net/jpg/02/47/40/98/360_F_247409832_pPugfgU5cKLsrH5OCJRMn5JTcy2L1Rrg.jpg") }
 
   before { subject.save }
