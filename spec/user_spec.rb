88f955712344d426d470ec7df1f4568db16ead36
unknown <kashif_saleem_23@hotmail.com>
2023-03-31 11:13:44 +0500

    Write unit test for User model

---
+++ spec/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+Rsepc.describe User, type: :model do
+  subject { User.new(name: "John", Bio:"Anything", photo:"https://t3.ftcdn.net/jpg/02/47/40/98/360_F_247409832_pPugfgU5cKLsrH5OCJRMn5JTcy2L1Rrg.jpg") }
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts_counter should be an integer' do
+    subject.posts_counter = "string"
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts_counter should be greater than or equal to 0' do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
+end
