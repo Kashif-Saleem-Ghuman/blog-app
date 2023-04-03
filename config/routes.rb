57dc5210b07a9e62f7496162e577dea8fa217610
unknown <kashif_saleem_23@hotmail.com>
2023-04-03 22:50:16 +0500

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:index,:show]
-  resources :posts, only: [:index,:show]
+  resources :users, only: [:index,:show] do
+    resources :posts, only: [:index,:show]
+  end
 
 end
