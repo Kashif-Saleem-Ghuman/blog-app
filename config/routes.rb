b149e893654a59fbf826dcaff52b1ac79e7490d9
unknown <kashif_saleem_23@hotmail.com>
2023-04-03 22:41:28 +0500

    Add routes for posts and index

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index,:show]
+  resources :posts, only: [:index,:show]
+
 end
