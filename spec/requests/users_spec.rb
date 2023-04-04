1c12685871627fecd3f1c24ff1e703fe6f3881ab
unknown <kashif_saleem_23@hotmail.com>
2023-04-04 15:39:45 +0500

    Add user controller specs

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe '/Users', type: :request do
+  describe 'Get/index' do
+    before(:example) do
+      get '/users'
+    end
+    it 'renders a successful response' do
+      expect(response).to be_successful
+    end
+    it 'contains the correct text' do
+      expect(response.body).to include('Here is a list of all users')
+    end
+  end
+
+  describe 'Get/show' do
+    before(:example) do
+      get '/users/1'
+    end
+    it 'renders a successful response' do
+      expect(response).to be_successful
+    end
+    it 'contains the correct text' do
+      expect(response.body).to include('Here are the detials about User XYZ')
+    end
+  end
+end
