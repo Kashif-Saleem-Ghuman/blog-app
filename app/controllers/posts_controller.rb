44563574eafe8e17af6daf01f93f8c4d304dbfd9
unknown <kashif_saleem_23@hotmail.com>
2023-04-05 16:24:25 +0500

    Intial setup for the all posts page for a given user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @users = User.find(params[:user_id])
   end
 
   def show
     @post = Post.find(params[:id])
+    @users = User.find(params[:id])
+    @comments = @post.comments
+    @likes = @post.likes
   end
 end
