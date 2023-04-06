0b142eb15851979255fd0cfed5a6dc19dd7ba8e1
unknown <kashif_saleem_23@hotmail.com>
2023-04-06 16:38:06 +0500

    Add new and create methods for posts in the post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,26 @@ class PostsController < ApplicationController
     @comments = @post.comments
     @likes = @post.likes
   end
+
+
+  def new
+    @post = Post.new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to root_path notice: "Post created successfully"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+      params.require(:post).permit(:title, :text)
+  end
+
 end
