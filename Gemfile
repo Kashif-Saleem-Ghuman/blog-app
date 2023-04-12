3ce1d738d08df77ad73c0d3bded3be85630ebe58
unknown <kashif_saleem_23@hotmail.com>
2023-04-12 23:18:40 +0500

    Add email confirmation

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ ruby '3.2.1'
 
 gem 'devise'
 
+gem 'letter_opener', group: :development
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.4', '>= 7.0.4.3'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name])
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :bio, :email, :password, :password_confirmation, :current_password)
+      u.permit(:name, :Bio, :email, :password, :password_confirmation, :current_password)
     end
   end
   # def current_user(id)
