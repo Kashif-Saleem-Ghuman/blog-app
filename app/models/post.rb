649c9adac265a605f7b85765e145b558f1fe31bb
Harrison Njuguna <noqsnoqs@gmail.com>
2023-04-13 13:03:35 +0300

    Add delete button

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  has_many :comments, foreign_key: 'post_id'
-  has_many :likes, foreign_key: 'post_id'
+  has_many :comments, foreign_key: 'post_id', dependent: :destroy
+  has_many :likes, foreign_key: 'post_id', dependent: :destroy
   belongs_to :author, class_name: 'User'
 
   after_save :update_posts_counter
