71ae3949acb5a0560e2cd861f5da1911846cf96c
unknown <kashif_saleem_23@hotmail.com>
2023-04-15 01:16:12 +0500

    Refactor to fix issues in User controller for API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,18 @@
-class Api::V1::Users::UsersController < ApplicationController
-  module Api
+module Api
     module V1
       class UsersController < ApplicationController
         def index
           @all_users = User.all
           @user = current_user
+
+          render json: @all_users
         end
 
         def show
           @user = User.find(request.parameters[:id])
+          render json: @user
         end
       end
     end
   end
-end
+
