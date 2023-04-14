01e28060d1215f6085e162a003e40c26c20768b7
unknown <kashif_saleem_23@hotmail.com>
2023-04-15 01:16:41 +0500

    Update ruby version

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   after_save :update_posts_counter
+  after_destroy :update_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
